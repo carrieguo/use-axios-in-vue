8cbd3a81248dd5dc0b17e13aa50773bdd9cf6163
carrieguo <593774767@qq.com>
2019-08-04 22:54:59 +0800

    项目搭建

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="test">{{text}}</div>
+</template>
+
+//控制显示内容如何变化
+<script>
+  export default {
+    data() {
+      return {
+        text: 'abc'
+      }
+    }
+  }
+</script>
+
+<style>
+  #test {
+    color: red;
+  }
+</style>
