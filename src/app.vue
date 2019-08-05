665a1454129146d7c3cd9a9cf049092b60b65840
carrieguo <593774767@qq.com>
2019-08-05 21:30:19 +0800

    get 请求成功

--- src/app.vue
+++ src/app.vue
@@ -4,11 +4,30 @@
 
 //控制显示内容如何变化
 <script>
+  import axios from "axios";
   export default {
     data() {
       return {
         text: 'abc'
       }
+    },
+    created: function() {
+      // HTTP://localhost:8080/data.json?id=12
+      axios.get('../public/test.json', {
+        params: {id: 12}
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        //另一种写法
+        axios({
+          method: 'GET',
+          url: "/public/test.json",
+          params: {id: 12}
+        })
     }
   }
 </script>
