0d098d2a786087a5ea93ecd0f2a9723ee5a76cd5
carrieguo <593774767@qq.com>
2019-08-06 22:14:35 +0800

    post and put method

--- README.md
+++ README.md
@@ -21,7 +21,8 @@
 
 ## Axios请求方法及别名（get方法）
 > * `get` 获取数据
-> * `post`  提交数据（表单提交+文件上传）
+> * `post`  提交数据（`form-data`表单提交: 图片+文件上传； `application/json`）
+`Request Headers` 中 `Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryO24RQrpc59b2ZIe3` boundary 相当于把传输的数据编码了
 > * `put`  更新数据（所有数据 推送到后端）
 > * `patch` 更新数据（只将修改的数据推送到后端）
 > * `delete` 删除数据
--- src/app.vue
+++ src/app.vue
@@ -12,6 +12,7 @@
       }
     },
     created: function() {
+      //get 请求
       // HTTP://localhost:8080/data.json?id=12
       axios.get('../public/test.json', {
         params: {id: 12}
@@ -22,12 +23,36 @@
         .catch(function (error) {
           console.log(error);
         });
-        //另一种写法
+        //get请求的另一种写法
         axios({
           method: 'GET',
           url: "/public/test.json",
           params: {id: 12}
-        })
+        });
+
+        //post 请求
+        let data = {id: 12};
+        axios.post('/post', data).then(function(response) {
+          console.log(response);
+          });
+        //post请求的另一种写法
+        axios({
+          method: 'post',
+          url: '/post',
+          data: data
+        }).then(function(res) {
+          console.log(res);
+        });
+        //post 请求，传递formData
+        let fd = new FormData();
+        for(let key in data) {
+          fd.append(key, data[key]);
+        }
+        axios.post('/post', fd).then((res) => console.log(res));
+        //put 请求
+        axios.put('/put', data).then((res) => console.log(res));
+        //patch 请求
+        axios.patch('/patch', data).then((res) => console.log(res));
     }
   }
 </script>
