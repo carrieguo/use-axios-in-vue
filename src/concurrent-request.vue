e725f1f59d74b9ed8d6a574e8031a73016b27d29
carrie.guo <593774767@qq.com>
2019-08-10 17:28:15 +0800

    axios instance

---
+++ src/concurrent-request.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+
+//并发请求
+<script>
+  import axios from "axios";
+
+  export default {
+    name: 'axios2 并发请求',
+    created: function() {
+      axios.all([
+        axios.get('../public/test.json'),
+        axios.get('../public/test1.json')
+      ]).then(
+        axios.spread(
+          (dataRes, data2Res) => {
+            console.log('并发请求');
+            console.log(dataRes, data2Res);
+          }
+        )
+      );
+    }
+  }
+</script>
+
+<style>
+</style>
